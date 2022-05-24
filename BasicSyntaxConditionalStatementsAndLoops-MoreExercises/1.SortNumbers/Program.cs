fc6a51d61b00104af8f9e5d8b13f179e397b672a
Stefan Rusev <StefanGRusev91@gmail.com>
2022-05-24 17:43:53 +0300

    basicSyntaxConditionalStatementsAndLoops-MoreExercises

---
+++ BasicSyntaxConditionalStatementsAndLoops-MoreExercises/1.SortNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfnumbers = new List<int>();
+            int integerOne = int.Parse(Console.ReadLine());
+            listOfnumbers.Add(integerOne);
+            int integerTwo = int.Parse(Console.ReadLine());
+            listOfnumbers.Add(integerTwo);
+            int integerThree = int.Parse(Console.ReadLine());
+            listOfnumbers.Add(integerThree);
+
+            listOfnumbers.Sort();
+            listOfnumbers.Reverse();
+            listOfnumbers.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ BasicSyntaxConditionalStatementsAndLoops-MoreExercises/2.EnglNameOfLastDigit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _2.EnglNameOfLastDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            Console.WriteLine(LastDigit(number));
+
+        }
+
+        static string LastDigit(string num)
+        {
+            int lastDigit = num[num.Length - 1];
+            string digit = string.Empty;
+
+            digit = lastDigit switch
+            {
+                '0' => "zero",
+                '1' => "one",
+                '2' => "two",
+                '3' => "three",
+                '4' => "four",
+                '5' => "five",
+                '6' => "six",
+                '7' => "seven",
+                '8' => "eight",
+                '9' => "nine",
+                _ => "error",
+            };
+
+            return digit;
+
+        }
+    }
+}
