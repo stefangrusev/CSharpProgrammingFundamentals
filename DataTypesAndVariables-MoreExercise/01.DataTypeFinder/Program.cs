e0f0a56c444ea82a05a04ac1e7bd44ff4f02c6f7
Stefan Rusev <StefanGRusev91@gmail.com>
2022-05-31 19:59:00 +0300

    dataTypesAndVariables-MoreExercises

---
+++ DataTypesAndVariables-MoreExercise/01.DataTypeFinder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _01.DataTypeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var possibleDataType = Console.ReadLine();
+
+            while (possibleDataType != "END")
+            {
+                if (!Int32.TryParse(possibleDataType, out _))
+                {
+                    if (!float.TryParse(possibleDataType, out _))
+                    {
+                        if (char.TryParse(possibleDataType, out _))
+                        {
+                            Console.WriteLine($"{possibleDataType} is character type");
+                        }
+                        else if (bool.TryParse(possibleDataType, out _))
+                        {
+                            Console.WriteLine($"{possibleDataType} is boolean type");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{possibleDataType} is string type");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{possibleDataType} is floating point type");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{possibleDataType} is integer type");
+                }
+
+                possibleDataType = Console.ReadLine();
+            }
+        }
+    }
+}
