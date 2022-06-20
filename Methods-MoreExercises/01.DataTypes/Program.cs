e5d0d2ab3633d31005d2dceaaa2fb12846d8c656
Stefan Rusev <StefanGRusev91@gmail.com>
2022-06-20 21:39:41 +0300

    Methods-MoreExercises

--- Methods-MoreExercises/01.DataTypes/Program.cs
+++ Methods-MoreExercises/01.DataTypes/Program.cs
@@ -6,7 +6,41 @@ namespace _01.DataTypes
     {
         static void Main(string[] args)
         {
+            string dataType = Console.ReadLine();
+            string theResult = string.Empty;
 
+            if (dataType == "int")
+            {
+                int inputAsInt = int.Parse(Console.ReadLine());
+                theResult = OverloadedInputOperations(inputAsInt);
+            }
+            else if (dataType == "real")
+            {
+                double inputAsDouble = double.Parse(Console.ReadLine());
+                theResult = $"{OverloadedInputOperations(inputAsDouble):f2}"; ;
+            }
+            else if (dataType == "string")
+            {
+                string inputAsString = Console.ReadLine();
+                theResult = OverloadedInputOperations(inputAsString);
+            }
+            Console.WriteLine(theResult);
+        }
+
+        private static string OverloadedInputOperations(string inputAsString)
+        {
+           return $"${inputAsString}$";
+        }
+
+        private static double OverloadedInputOperations(double inputAsDouble)
+        {
+            double resultForDouble = inputAsDouble * 1.5;
+            return resultForDouble;
+        }
+
+        private static string OverloadedInputOperations(int inputAsInt)
+        {
+            return (inputAsInt * 2).ToString();
         }
     }
 }
